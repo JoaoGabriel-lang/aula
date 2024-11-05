2980690498289830b61724bbe0cc1d06a96c9305
JoaoGabriel-lang <jgabriel.rochaalves@gmail.com>
2024-11-05 13:53:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let numero = prompt("Digite um número:");
+
+
+numero = Number(numero);
+
+
+if (numero > 0) {
+    console.log("O número é maior que zero.");
+} else if (numero < 0) {
+    console.log("O número é menor que zero.");
+} else {
+    console.log("O número é zero.");
+}
